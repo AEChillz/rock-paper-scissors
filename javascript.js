54dd148a14abeb9a844b4edd99d2ed693ed01f42
AEChillz <183168781+AEChillz@users.noreply.github.com>
2025-02-21 16:05:41 +0800

    Looped playGame func

--- javascript.js
+++ javascript.js
@@ -15,76 +15,60 @@ function getHumanChoice () {
   return humanPrompt; 
 }
 
-let humanScore = 0;
-let computerScore = 0;
 
+
+const humanWon = "you have the highest score, you win!";
+const computerWon = "compute has the highest score, computer wins!"
 const humanWin = "human wins";
 const computerWin = "computer wins";
 const noWin = "no one wins.";
-const humanWon = "you have the highest score, you win!";
-const computerWon = "compute has the highest score, computer wins!"
 
 
-function playRound(humanChoice, computerChoice) {
+function playGame () {
+  let humanScore = 0;
+  let computerScore = 0;
+ const playRounds = function playRound(humanChoice, computerChoice) {
   if (humanChoice === "rock" && computerChoice === "rock") {
    console.log("both chose rock " + noWin + "");
   } else if (humanChoice === "rock" && computerChoice === "paper") {
-    computerScore++;
     console.log("computer chose paper " + computerWin + "");
+    computerScore++;
   } else if (humanChoice === "rock" && computerChoice === "scissors") {
-    humanScore++;
     console.log("computer chose scissors " + humanWin + "" );
-  } else if (humanChoice === "paper" && computerChoice === "rock") {
     humanScore++;
+  } else if (humanChoice === "paper" && computerChoice === "rock") {
     console.log("computer chose rock " + humanWin + "");
+    humanScore++;
   } else if (humanChoice === "paper" && computerChoice === "paper") {
     console.log("both chose paper" + noWin + "" );
   } else if (humanChoice === "paper" && computerChoice === "scissors") {
-    computerScore++;
     console.log("computer chose scissors " + computerWin + "");
-  } else if (humanChoice === "scissors" && computerChoice === "rock") {
     computerScore++;
+  } else if (humanChoice === "scissors" && computerChoice === "rock") {
     console.log("computer chose rock " + computerWin + "" );
+    computerScore++;
   } else if (humanChoice === "scissors" && computerChoice === "paper") {
-    humanScore++;
     console.log("computer chose paper " + humanWin + "");
+    humanScore++;
   } else if (humanChoice === "scissors" && computerChoice === "scissors") {
     console.log("both chose scissors " + noWin + "" );
   }
-}
+}  
 const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
 
-playRound(humanSelection, computerSelection);
+playRounds(humanSelection, computerSelection); 
 
-console.log("your score is " + humanScore + "");
-console.log("computers score is " + computerScore + "");
-let rounds = 0;
 
-function playGame () {
-   switch (rounds) {
-    case 1:
-    rounds++
-    console.log(rounds)
-    break;
-    case 2:
-    rounds++
-    console.log(rounds)
-    break;
-    case 3:
-    rounds++
-    console.log(rounds)
-    break;
-    case 4:
-    rounds++
-    console.log(rounds)
-    break;
-    case 5:
-    rounds++
-    console.log(rounds)
-    break;
-   }
 }
+for (let i = 0; i < 5; i++) {
+  playGame(i);
+}
+playGame();
+
+
+
+
 
 
 
